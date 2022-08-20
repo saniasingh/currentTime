ef67b008ede787ac8e9da82ba36ea62e93dc2edf
Sania Singh <saniasingh4in@gmail.com>
2022-08-20 18:47:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import React, { useState } from 'react';
+import { ReactDOM } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import currentTime from "./components/time";
 
 function App() {
+
+ setInterval(TIME, 1000);
+
+ const now = new Date().toLocaleString();
+ 
+ const [time, setTime] = useState(now)
+
+ function TIME() {
+  const newTime = new Date().toLocaleString();
+  return setTime(newTime);
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>{time}</h1>
+      <button type='button' className='btn btn-primary' onClick={TIME}>Show Time</button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/time.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// const [time] = useState()
+
+// let Time = new Date().toLocaleTimeString();
+// console.log(Time);
+
+function currentTime() {
+
+    // let Time = new Date().toLocaleTimeString();
+
+
+
+    return (
+        // <div>
+
+<h1>hello</h1>
+/* <button onClick={Time}>Show Time</button> */
+
+
+
+        // </div>
+    )
+}
+
+export default currentTime;
